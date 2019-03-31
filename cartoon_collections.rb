5107835da19e269a460714fd0c17d2b97974bd64
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-31 23:58:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,16 @@ def roll_call_dwarves# code an argument here
 end 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(cap)
   # Your code here
-  
+    cap.collect do |n|
+    n.capitalize + "!"
+end
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
